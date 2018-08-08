a0efa23e88f2a445c81a08c6514f2b92a1f96fde
Katrina Ariola <kat3ariola@gmail.com>
2018-08-08 15:45:02 +0900

    updated restaurant and review input sheets

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :restaurant
   validates :content, presence: true
   validates_associated :restaurant
-  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+  validates :rating, inclusion: { in: [1, 2, 3, 4, 5] }
   validates :rating, numericality: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Sushirito',
+    address:      '101 Montgomery St, San Francisco CA',
+    phone_number: 4155321234,
+    category:     'japanese'
+  },
+  {
+    name:         'Working Girls',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: 4155512343,
+    category:     'french'
+  },
+  {
+    name:         'El Farolito',
+    address:      'One Montgomery, San Francisco CA',
+    phone_number: 4154326200,
+    category:     'italian'
+  },
+  {
+    name:         'R&G',
+    address:      '1 Tokyo Street, Tokyo, Japan',
+    phone_number: 4154326222,
+    category:     'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
